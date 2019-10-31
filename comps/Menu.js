00ab1379a920412fe1bfe97230c9dade45d46c8e
Ajay <ajajaj2807@gmail.com>
2019-10-31 06:07:36 +0530

    Initial Commit

---
+++ comps/Menu.js
@@ -0,0 +1,50 @@
+import Link from 'next/link'
+
+const Menu = (props) => {
+    return (
+        <div className="menu-container">
+            <div className="close-btn" onClick={ props.close }>
+                <i class="fas fa-times"></i>
+            </div>
+            <div className="menu-info">
+            <span className="mail">
+                Send me a 'Hi!' at: <br/> <b>ajajaj2807@gmail.com</b>
+            </span>
+            <span className="address">
+                Find me at: <br/> 
+                <b> MMM Hall, IIT Kharagpur <br/> WB, India 721302 </b>
+            </span>
+            <div className="menu-social">
+            <a href="https://www.fb.com/ajajaj2807">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="https://www.instagram.com/_ajajaj_">
+                        <i class="fab fa-instagram"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/ajajaj2807">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                        <a href="https://www.github.com/ajajaj2807">
+                            <i class="fab fa-github"></i>
+                        </a>
+            </div>
+            </div>
+            <div className="nav-items">
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+                <Link href="/about">
+                    <a>About</a>
+                </Link>
+                <Link href="/cv">
+                    <a>Curriculum Vitae</a>
+                </Link>
+                <Link href="/work">
+                    <a>Work</a>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Menu
---
+++ pages/work.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class work extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Page under construction</h2>
+            </div>
+        )
+    }
+}
+
+export default work
