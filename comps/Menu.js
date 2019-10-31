9585b8af1c60d783223d6ec65712b1bf93322c80
Ajay <ajajaj2807@gmail.com>
2019-10-31 12:04:07 +0530

    Minor Mobile UI Changes

--- comps/Menu.js
+++ comps/Menu.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import '../styles/main.scss'
 
 const Menu = (props) => {
     return (
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,21 @@
+import Menu from '../comps/Menu'
+import { useState } from 'react'
 
-const About = () => {
+const about = () => {
+    
+    const [menu, setMenu] = useState(0)
+    
     return(
         <div>
+            <button className="menu-btn" onClick={ () => setMenu(!menu) }>
+            <i class="fas fa-bars"></i>
+        </button>
+        { menu ? <Menu close= { () => setMenu(!menu) } /> : <> </>}
             <h1>About Section</h1>
+            <p>
+                Work in Progress
+            </p>
         </div>
     )
 }
-export default About
+export default about;
