d5db9563dae896a23a051e0975954b322314ef8e
Ajay <ajajaj2807@gmail.com>
2020-04-24 15:23:17 +0530

    quarantine goals

--- comps/Menu.js
+++ comps/Menu.js
@@ -1,42 +1,44 @@
-import '../styles/main.scss'
+import "../styles/main.scss";
 
 const Menu = (props) => {
-    return (
-        <div className="menu-container">
-            <div className="close-btn" onClick={ props.close }>
-                <i class="fas fa-times"></i>
-            </div>
-            <div className="menu-info">
-            <span className="mail">
-                Send me a 'Hi!' at: <br/> <b>ajajaj2807@gmail.com</b>
-            </span>
-            <span className="address">
-                Find me at: <br/> 
-                <b> MMM Hall, IIT Kharagpur <br/> WB, India 721302 </b>
-            </span>
-            <div className="menu-social">
-            <a href="https://www.fb.com/ajajaj2807">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="https://www.instagram.com/_ajajaj_">
-                        <i class="fab fa-instagram"></i>
-                        </a>
-                        <a href="https://www.linkedin.com/in/ajajaj2807">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
-                        <a href="https://www.github.com/ajajaj2807">
-                            <i class="fab fa-github"></i>
-                        </a>
-            </div>
-            </div>
-            <div className="nav-items">
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/cv">Curriculum Vitae</a>
-                <a href="/work">Work</a>
-            </div>
+  return (
+    <div className="menu-container">
+      <div className="close-btn" onClick={props.close}>
+        <i class="fas fa-times"></i>
+      </div>
+      <div className="menu-info">
+        <span className="mail">
+          Send me a 'Hi!' at: <br /> <b>ajajaj2807@gmail.com</b>
+        </span>
+        <span className="address">
+          Find me at: <br />
+          <b>
+            {" "}
+            MMM Hall, IIT Kharagpur <br /> WB, India 721302{" "}
+          </b>
+        </span>
+        <div className="menu-social">
+          <a href="https://www.fb.com/ajajaj2807">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://www.instagram.com/_ajajaj_">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ajajaj2807">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://www.github.com/ajajaj2807">
+            <i class="fab fa-github"></i>
+          </a>
         </div>
-    )
-}
+      </div>
+      <div className="nav-items">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/cv">Curriculum Vitae</a>
+      </div>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,6 @@ import Menu from "../comps/Menu";
 import { useState, useEffect } from "react";
 import "../styles/about.scss";
 import Head from "next/head";
-import Fade from "react-reveal/Fade";
 
 const about = () => {
   const [menu, setMenu] = useState(0);
@@ -19,54 +18,7 @@ const about = () => {
       {menu ? <Menu close={() => setMenu(!menu)} /> : <> </>}
 
       <div className="about-wrapper">
-        <div className="card-80 about-intro">
-          <span className="big-title">
-            <Fade bottom big cascade>
-              So, you want to know about me? <br />
-              Let's dive in!
-            </Fade>
-          </span>
-        </div>
-        <div className="card-80 about-name">
-          <img src="../static/about-01.svg" />
-          <Fade right>
-            <div className="an-text">
-              <span className="big-title">
-                My name is Oliver Queen <br />
-              </span>
-              <span>Just kidding! My parents named me Ajay</span>
-            </div>
-          </Fade>
-        </div>
-
-        <div className="card-80 about-edu">
-          <img src="../static/about-02.svg" />
-          <Fade left>
-            <div className="an-text">
-              <span className="big-title">
-                I'm a student! <br />
-              </span>
-              <span>
-                I am a 2nd year Undergraduate studying at Indian <br />{" "}
-                Institute of Technology, Kharagpur
-              </span>
-            </div>
-          </Fade>
-        </div>
-        <div className="card-80 about-int">
-          <img src="../static/about-03.svg" />
-          <Fade right>
-            <div className="an-text">
-              <span className="big-title">
-                I love good music <br />
-              </span>
-              <span className="about-subs">
-                I like to code and listen to good <br />
-                beats whenever, wherever
-              </span>
-            </div>
-          </Fade>
-        </div>
+        <h2>Under Construction</h2>
       </div>
     </div>
   );
