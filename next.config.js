e67ca5edd81ca3c7639b20a460d9402ee42e5cce
Ajay <ajajaj2807@gmail.com>
2020-05-15 12:53:58 +0530

    fixed dynamic routing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
-const withSass = require('@zeit/next-sass')
-const withCSS = require('@zeit/next-css')
+const withSass = require("@zeit/next-sass");
+const withCSS = require("@zeit/next-css");
+
+const client = require("./client");
+
+module.exports = {
+  // Make sure that your node enviroment supports async/await
+  exportPathMap: async function (defaultPathMap) {
+    const path = await client
+      // get all the posts and return those with slugs
+      .fetch('*[_type == "details"].slug.current')
+      .then((data) =>
+        // use reduce to build an object with routes
+        // and select the blog.js file, and send in the
+        // correct query paramater.
+        data.reduce(
+          (acc, slug) => ({
+            "/": { page: "/" },
+            ...acc,
+            [`/details/${slug}`]: { page: "/details", query: { slug } },
+          }),
+          {}
+        )
+      )
+      .catch(console.error);
+    return path;
+  },
+};
 
 module.exports = withCSS(withSass());
