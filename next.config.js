c286d51bad3938bb345be696bfc24209e99af708
Ajay <ajajaj2807@gmail.com>
2020-05-15 13:26:16 +0530

    Changed hosting to Vercel

--- next.config.js
+++ next.config.js
@@ -1,30 +1,4 @@
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
-
-const client = require("./client");
-
-module.exports = {
-  // Make sure that your node enviroment supports async/await
-  exportPathMap: async function (defaultPathMap) {
-    const path = await client
-      // get all the posts and return those with slugs
-      .fetch('*[_type == "details"].slug.current')
-      .then((data) =>
-        // use reduce to build an object with routes
-        // and select the blog.js file, and send in the
-        // correct query paramater.
-        data.reduce(
-          (acc, slug) => ({
-            "/": { page: "/" },
-            ...acc,
-            [`/details/${slug}`]: { page: "/details", query: { slug } },
-          }),
-          {}
-        )
-      )
-      .catch(console.error);
-    return path;
-  },
-};
+const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS(withSass());
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 const Index = () => {
   return (
-    <h3>Domain owned by Ajay Yadav. Go to /corona for Corona Live Updates</h3>
+    <h3>
+      Domain owned by Ajay Yadav. Go to /corona for Corona Live Updates. Made
+      with NextJS
+    </h3>
   );
 };
 
